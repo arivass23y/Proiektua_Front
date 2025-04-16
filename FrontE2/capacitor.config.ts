25c0aee6754560584bb70665f7ae29544603bea6
mokelClase <mostolozagan23@fpsanturtzilh.eus>
2025-04-16 14:03:12 +0200

    popup google

--- FrontE2/capacitor.config.ts
+++ FrontE2/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'FrontE2',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    "GoogleAuth": {
+      "scopes": ["profile", "email"],
+      "serverClientId": "862214888792-r4fau3jo13m2mhurf203k34st03i3oma.apps.googleusercontent.com",
+      "forceCodeForRefreshToken":true
+    }
+  }
 };
 
 export default config;
--- FrontE2/src/app/login/login.page.ts
+++ FrontE2/src/app/login/login.page.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { LoginServiceService } from '../zerbitzuak/login-service.service';
 import { firstValueFrom } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { isPlatform } from '@ionic/angular';
 declare const google: any;
 
 
@@ -19,7 +21,15 @@ export class LoginPage implements OnInit {
   loginMessageType: 'success' | 'error' = 'error'; // Para controlar el tipo de mensaje (exito o error)
   selectedLanguage: string = 'es';
 
-  constructor(private router: Router, private loginService: LoginServiceService, private translate: TranslateService) {}
+  constructor(private router: Router, private loginService: LoginServiceService, private translate: TranslateService) {
+    if(!isPlatform('capacitor')){
+      GoogleAuth.initialize({
+        clientId:'862214888792-r4fau3jo13m2mhurf203k34st03i3oma.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+        grantOfflineAccess:true
+      });
+    }
+  }
 
   ngOnInit() {
     this.translate.setDefaultLang(this.selectedLanguage);
@@ -34,33 +44,36 @@ export class LoginPage implements OnInit {
     this.changeLanguage();
   }
   
+  user='';
   
-  onGoogleLogin() {
-    google.accounts.id.initialize({
-      client_id: '194803716129-2rj4mdmmoktc1o79k0q45kfnv1a5222f.apps.googleusercontent.com', // Sustituye con el tuyo
-      callback: async (response: any) => {
-        const idToken = response.credential;
+  async onGoogleLogin() {
+    this.user= (await GoogleAuth.signIn()).email;
+    console.log(this.user);
+    // google.accounts.id.initialize({
+    //   client_id: '194803716129-2rj4mdmmoktc1o79k0q45kfnv1a5222f.apps.googleusercontent.com', // Sustituye con el tuyo
+    //   callback: async (response: any) => {
+    //     const idToken = response.credential;
   
-        try {
-          const success = await firstValueFrom(this.loginService.loginWithGoogle(idToken));
+    //     try {
+    //       const success = await firstValueFrom(this.loginService.loginWithGoogle(idToken));
   
-          if (success) {
-            this.loginMessage = this.translate.instant('login.messageOk');
-            this.loginMessageType = 'success';
-            this.router.navigate(['/home']);
-          } else {
-            this.loginMessage = this.translate.instant('login.messageFail');
-            this.loginMessageType = 'error';
-          }
-        } catch (error) {
-          console.error("Error en Google login:", error);
-          this.loginMessage = this.translate.instant('login.messageNotOk');
-          this.loginMessageType = 'error';
-        }
-      }
-    });
+    //       if (success) {
+    //         this.loginMessage = this.translate.instant('login.messageOk');
+    //         this.loginMessageType = 'success';
+    //         this.router.navigate(['/home']);
+    //       } else {
+    //         this.loginMessage = this.translate.instant('login.messageFail');
+    //         this.loginMessageType = 'error';
+    //       }
+    //     } catch (error) {
+    //       console.error("Error en Google login:", error);
+    //       this.loginMessage = this.translate.instant('login.messageNotOk');
+    //       this.loginMessageType = 'error';
+    //     }
+    //   }
+    // });
   
-    google.accounts.id.prompt(); // Esto abre el popup
+    // google.accounts.id.prompt(); // Esto abre el popup
   }
 
   async onLogin() {
